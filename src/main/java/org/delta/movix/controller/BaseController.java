0bef01fb3a26fa012f297ce304ce65ab34304774
Li Yi <yi.3.li@nokia-sbell.com>
2019-01-30 15:18:23 +0800

    Add cors support.

--- src/main/java/org/delta/movix/controller/BaseController.java
+++ src/main/java/org/delta/movix/controller/BaseController.java
@@ -2,7 +2,9 @@ package org.delta.movix.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 public abstract class BaseController {
 
     public Logger logger = LoggerFactory.getLogger(getClass());
